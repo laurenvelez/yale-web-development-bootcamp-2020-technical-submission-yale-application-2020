ed0ad2f76bc72fad761caf2e81407bb53632453b
Lauren Velez <laurenmiavelez@gmail.com>
2020-01-19 01:07:32 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -5,9 +5,9 @@ AVERAGE_HEIGHT_IN_INCHES = 67
 
 name = "Lauren"
 age = 19
-favorite_food = "Chicken"
+favorite_food = "chicken"
 height_in_inches = 64
-height_message = AVERAGE_HEIGHT_IN_INCHES > height_in_inches ? :you_are_taller_than_average : :you_are_not_taller_than_average
+height_message = AVERAGE_HEIGHT_IN_INCHES > height_in_inches ? : : You are_taller_than_average : :you_are_not_taller_than_average
 
 ## Do not modify below this line!
 
